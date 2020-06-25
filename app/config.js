afb22644c476d8088b3df256e97a085fb7812b9a
Ben Fraser <ben.fraser@engineering.digital.dwp.gov.uk>
2020-06-25 16:29:46 +0100

    Example views etc

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Prototype Kit - view versioning functionality',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,12 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.post('/default/views-out-of-hand-submit', (req, res) => {
+  let areViewsOutOfHand = req.session.data['views-out-of-hand']
+  if (areViewsOutOfHand === 'yes') {
+    res.redirect('/default/use')
+  } else {
+    res.redirect('/default/ok')
+  }
+})
